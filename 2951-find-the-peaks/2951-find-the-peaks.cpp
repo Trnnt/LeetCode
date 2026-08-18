c1b3b2ab16b04ed15dc8faf209c8377bb5fdfda1
Nishant kumar <118263496+Trnnt@users.noreply.github.com>
2026-08-18 11:20:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& m) {
+        vector<int> ans;
+        for(int i=1; i<m.size(); i++){
+            if( m[i-1] < m[i] && m[i] > m[i+1]){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
