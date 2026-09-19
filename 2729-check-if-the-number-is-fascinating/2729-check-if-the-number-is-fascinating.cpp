5f67edd98e75cf67125835d9f6b04be096cc1e49
Nishant kumar <118263496+Trnnt@users.noreply.github.com>
2026-09-19 22:21:57 +0530

    Time: 0 ms (100%), Space: 10 MB (21.53%) - LeetHub

---
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isFascinating(int n) {
+        long long ans = stoll(to_string(n) + to_string(2*n) + to_string(3*n));
+
+        int freq[10] ={0};
+        while(ans > 0){
+            int digit = ans%10;
+            ans  = ans /10;
+            freq[digit]++;
+        }
+        if(freq[0] > 0){
+            return false;
+        }
+        for(int i=1; i<=9; i++){
+            if(freq[i] !=1){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
